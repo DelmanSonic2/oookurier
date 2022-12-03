b867ee6da4a0fd9dcaee889d20a067d5021a192b
Delmansonic2 <delmansonic@gmail.com>
2022-12-03 12:20:42 +0300

    Нарисованы формы регистрации и авторизации, к ним подключены контроллеры, пока без действий

---
+++ src/main/java/com/program/oookurier/ViewsControllers/AuthSceneController.java
@@ -0,0 +1,25 @@
+package com.program.oookurier.ViewsControllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class AuthSceneController {
+
+    @FXML
+    private TextField lognFleld;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    void auth(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toRegister() {
+
+    }
+}
---
+++ src/main/java/com/program/oookurier/ViewsControllers/RegisterSceneController.java
@@ -0,0 +1,32 @@
+package com.program.oookurier.ViewsControllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class RegisterSceneController {
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private TextField phoneField;
+
+    @FXML
+    void register(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toAuth() {
+
+    }
+
+}
